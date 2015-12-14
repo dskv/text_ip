8c8790d87aaad09e50b9a0f0596054d3201ebb8d
Dan Kay <dskv@yahoo.com>
2015-12-14 15:20:40 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,74 @@
+sms_ip
+======
+
+Send a sms/text message using google voice to your phone when device (raspberry pi) boots and is assigned an ip address.
+
+The scrypt uses Google voice, so go set that up if you need to at www.google.com/voice
+
+To have the scrypt run automatically on boot add following command:
+	sudo nano /etc/rc.local
+	
+edit the rc.local file
+#--------------------------------------------------------------------
+\#!/bin/sh -e
+#
+# rc.local
+#
+# This script is executed at the end of each multiuser runlevel.
+# Make sure that the script will "exit 0" on success or any other
+# value on error.
+#
+# In order to enable or disable this script just change the execution
+# bits.
+#
+# By default this script does nothing.
+
+# Print the IP address
+_IP=$(hostname -I) || true
+if [ "$_IP" ]; then
+  printf "My IP address is %s\n" "$_IP"
+  python /home/pi/Desktop/sms_ip/get_ip.py 	#<--ADD THIS LINE
+fi
+exit 0
+#--------------------------------------------------------------------
+
+
+Also dont forget to edit the 'user_info.txt' file with your personal information
+
+
+Errors:
+
+gVoice needs to be installed, do the following if errors come up.
+
+#Command line Google Voice:
+
+#required tools:
+sudo apt-get install python python-simplejson python-setuptools
+sudo easy_install simplejson
+
+# if gvoice was installed previously, then uninstall it:
+sudo rm -r /usr/local/lib/python2.7/dist-packages/googlevoice
+sudo rm /usr/local/lib/python2.7/dist-packages/pygooglevoice*
+
+#download pygooglevoice:
+wget http://pygooglevoice.googlecode.com/files/pygooglevoice-0.5.tar.gz
+tar -xf pygooglevoice-0.5.tar.gz
+cd pygooglevoice
+
+# edit settings.py to match correct Google Voice URL on line # 22:
+nano googlevoice/settings.py
+
+#correct URL:
+LOGIN = 'https://accounts.google.com/ServiceLogin?service=grandcentral'
+#you may check if URL is linking to Google Voice login page in browser.
+#save and quit settings.py
+
+#install gvoice:
+sudo python setup.py install
+
+#Login and make call for the first time:
+gvoice # enter login email/pwd
+  gvoice> call #follow prompts and make a call
+  gvoice>send_sms # or s to send sms
+  gvoice>exit #quit gvoice
+#end
---
+++ text_ip.py
@@ -0,0 +1,83 @@
+#!/usr/bin/python
+"""
+
+Author: Dan Kay
+This repo is located at: https://github.com/dskv/
+Sends current internal and external IP on boot to your phone SMS/Text message. Works with wireless.
+
+"""
+
+from googlevoice import Voice
+import re
+import os
+import subprocess
+import urllib
+
+
+v = Voice()
+
+def main():
+	ver = get_os()
+	int_ip = get_internal_ip(ver)
+	ext_ip = get_external_ip()
+	text = 'Raspberry Pi: \n\n' + 'Internal: ' + int_ip + '\n' + 'External: ' + ext_ip 
+	print text
+	send_text(text)
+
+def get_os():
+	# Determine which OS distribution the system is running
+	version = os.uname()[0]
+	return version
+
+def get_internal_ip(version):
+	if version == 'Darwin': #I cant test for Darwin, edits might need to be made here
+		int_ip = subprocess.check_output(['/sbin/ifconfig','en0'])
+		int_ip = re.search('inet ([\d\.]*)', int_ip)
+	else:
+		int_ip = 'none'	
+
+	if version == 'Linux':
+		int_ip = subprocess.check_output(['/sbin/ifconfig','wlan0'])
+		int_ip = re.search('inet addr:([\d\.]*)', int_ip)
+		if int_ip:
+			int_ip = int_ip.group(1)
+		else:
+			print 'No wireless connection, trying LAN connection.'
+			int_ip = subprocess.check_output(['/sbin/ifconfig','eth0'])
+			int_ip = re.search('inet addr:([\d\.]*)', int_ip)		
+			int_ip = int_ip.group(1)
+	else:
+		int_ip = 'none'
+	
+	return int_ip
+
+def get_external_ip():
+	url = 'http://myexternalip.com/raw'
+	#do a bit of error handling if you are only working on Internal LAN connection
+	try:
+		ext_ip = urllib.urlopen(url).read()
+	except IOError:
+		print 'External URL IOError error.'
+		ext_ip = 'NONE'
+	except:
+		print 'External URL except error.'
+		ext_ip = 'NONE'
+	return ext_ip	
+
+def send_text(text):
+	# Reads a file 'user_info.txt' that you create with your login information. Of course you can ass your information directly here.
+	# EXAMPLE user_info.txt file:
+	# 1. email@gmail.com
+	# 2. YOUR_PASSWORD
+	# 3. 01234567890
+	user_info = open('/home/pi/Desktop/text_ip/user_info.txt', 'r')
+	user_name = user_info.readline() #read line one
+	user_pass = user_info.readline() #read line two
+	user_tele = user_info.readline() #read line three
+	user_info.close()
+	v.login(user_name, user_pass)
+	phoneNumber = user_tele
+	v.send_sms(phoneNumber, text)
+	v.logout()
+
+if __name__ == "__main__": main()
